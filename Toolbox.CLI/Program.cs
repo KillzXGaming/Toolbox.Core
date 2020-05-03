03d0915fe8e322122cda27274c471fd7ec193a67
KillzXGaming <millernathan35@gmail.com>
2020-05-03 15:29:27 -0400

    Push everything into seperate projects

---
+++ Toolbox.CLI/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Toolbox.Core;
+using Toolbox.Core.Hashes;
+using Toolbox.Core.IO;
+
+namespace Toolbox.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Runtime.ExecutableDir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+            var plugins = PluginManager.LoadPlugins();
+            foreach (var plugin in plugins)
+            {
+                Console.WriteLine($"Plugin {plugin.PluginHandler.Name}");
+
+                foreach (var file in plugin.FileFormats)
+                    Console.WriteLine($"file {file.Extension[0]}");
+            }
+
+            ExportImage("type_icon_00_normal.bflim");
+            ExportImage("ParamName_00.bflim");
+            ExportImage("sick_icon_01_mahi.bflim");
+
+            Console.Read();
+        }
+
+        static void ExportImage(string fileName)
+        {
+            var bflim = STFileLoader.OpenFileFormat(fileName) as STGenericTexture;
+            if (bflim == null) return;
+
+            Console.WriteLine($"tex {bflim.Name} {bflim.Format} {bflim.Width} {bflim.Height}");
+            bflim.GetBitmap().Save($"{bflim.Name}.png");
+        }
+    }
+}
